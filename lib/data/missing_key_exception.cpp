be863f57c1960ce7b52264dfb020bd5ae5c587da
Lukas Tietze <lukas.tietze@s2014.tu-chemnitz.de>
2018-08-13 14:25:59 +0200

    dinge ;)

---
+++ lib/data/missing_key_exception.cpp
@@ -0,0 +1,23 @@
+#include "data.hpp"
+
+std::string make_msg(const std::string &key)
+{
+    return util::format("Key \"%s\" could not be found!", key.c_str());
+}
+
+std::string make_msg(const std::string &key, const std::string &msg)
+{
+    return util::format("Key \"%s\" could not be found: \"%s\"", key.c_str(), msg.c_str());
+}
+
+util::missing_key_exception::missing_key_exception() : util::exception()
+{
+}
+
+util::missing_key_exception::missing_key_exception(const std::string &key) : util::exception(make_msg(key))
+{
+}
+
+util::missing_key_exception::missing_key_exception(const std::string &key, const std::string &msg) : util::exception(make_msg(key, msg))
+{
+}
