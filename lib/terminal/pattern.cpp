8076ca47ae36aab5b3434f0c2347278fc2430748
Lukas Tietze <lukas.tietze@s2014.tu-chemnitz.de>
2018-09-04 11:58:39 +0200

    Pattern

---
+++ lib/terminal/pattern.cpp
@@ -0,0 +1,135 @@
+#include "terminal/canvas.hpp"
+
+terminal::pattern::pattern() : items(),
+                               total_length(0)
+{
+}
+
+terminal::pattern::pattern(const pattern &copy) : items(copy.items),
+                                                  total_length(copy.total_length)
+{
+}
+
+terminal::pattern &terminal::pattern::push(char c)
+{
+    item newItem;
+    newItem.symbol = c;
+    newItem.length = 1;
+    newItem.use_custom_attributes = false;
+    newItem.use_custom_color = false;
+
+    this->total_length += 1;
+    this->items.push_back(newItem);
+}
+
+terminal::pattern &terminal::pattern::push(char c, int count)
+{
+    item newItem;
+    newItem.symbol = c;
+    newItem.length = count;
+    newItem.use_custom_attributes = false;
+    newItem.use_custom_color = false;
+
+    this->total_length += count;
+    this->items.push_back(newItem);
+}
+
+terminal::pattern &terminal::pattern::push(char c, const util::color &color)
+{
+    item newItem;
+    newItem.symbol = c;
+    newItem.length = 1;
+    newItem.use_custom_attributes = false;
+    newItem.use_custom_color = true;
+    newItem.custom_color = color;
+
+    this->total_length += 1;
+    this->items.push_back(newItem);
+}
+
+terminal::pattern &terminal::pattern::push(char c, int count, const util::color &color)
+{
+    item newItem;
+    newItem.symbol = c;
+    newItem.length = count;
+    newItem.use_custom_attributes = false;
+    newItem.use_custom_color = true;
+    newItem.custom_color = color;
+
+    this->total_length += count;
+    this->items.push_back(newItem);
+}
+
+terminal::pattern &terminal::pattern::push(char c, terminal::output_attribute a)
+{
+    item newItem;
+    newItem.symbol = c;
+    newItem.length = 1;
+    newItem.use_custom_attributes = true;
+    newItem.custom_attributes = a;
+    newItem.use_custom_color = false;
+
+    this->total_length += 1;
+    this->items.push_back(newItem);
+}
+
+terminal::pattern &terminal::pattern::push(char c, int count, terminal::output_attribute a)
+{
+    item newItem;
+    newItem.symbol = c;
+    newItem.length = count;
+    newItem.use_custom_attributes = true;
+    newItem.custom_attributes = a;
+    newItem.use_custom_color = false;
+
+    this->total_length += count;
+    this->items.push_back(newItem);
+}
+
+terminal::pattern &terminal::pattern::push(char c, const util::color &color, terminal::output_attribute a)
+{
+    item newItem;
+    newItem.symbol = c;
+    newItem.length = 1;
+    newItem.use_custom_attributes = true;
+    newItem.custom_attributes = a;
+    newItem.use_custom_color = true;
+    newItem.custom_color = color;
+
+    this->total_length += 1;
+    this->items.push_back(newItem);
+}
+
+terminal::pattern &terminal::pattern::push(char c, int count, const util::color &color, terminal::output_attribute a)
+{
+    item newItem;
+    item newItem;
+    newItem.symbol = c;
+    newItem.length = count;
+    newItem.use_custom_attributes = true;
+    newItem.custom_attributes = a;
+    newItem.use_custom_color = true;
+    newItem.custom_color = color;
+
+    this->total_length += count;
+    this->items.push_back(newItem);
+}
+
+void terminal::pattern::pop()
+{
+    if (!this->items.empty())
+    {
+        this->total_length -= this->items.back().length;
+        this->items.pop_back();
+    }
+}
+
+uint terminal::pattern::get_total_length()
+{
+    return this->total_length;
+}
+
+uint terminal::pattern::get_item_count()
+{
+    return this->items.size();
+}
