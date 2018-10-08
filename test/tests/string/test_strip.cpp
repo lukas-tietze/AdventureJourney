ac66a75938cfedd59f75f6d34dee1147409be5c8
Lukas Tietze <lukas.tietze@s2014.tu-chemnitz.de>
2018-10-08 08:56:44 +0200

    Test geändert

--- test/tests/string/test_strip.cpp
+++ test/tests/string/test_strip.cpp
@@ -13,18 +13,18 @@ int test::string_test::strip()
 
 int test::string_test::strip_front()
 {
-    assert::are_equal(std::string("abc "), util::strip(" abc "));
-    assert::are_equal(std::string("abc    "), util::strip("   abc    "));
-    assert::are_equal(std::string("abc\n\n   \t"), util::strip(" \t\nabc\n\n   \t"));
+    assert::are_equal(std::string("abc "), util::strip_front(" abc "));
+    assert::are_equal(std::string("abc    "), util::strip_front("   abc    "));
+    assert::are_equal(std::string("abc\n\n   \t"), util::strip_front(" \t\nabc\n\n   \t"));
 
     return 0;
 }
 
 int test::string_test::strip_back()
 {
-    assert::are_equal(std::string(" abc"), util::strip(" abc "));
-    assert::are_equal(std::string("   abc"), util::strip("   abc    "));
-    assert::are_equal(std::string(" \t\nabc"), util::strip(" \t\nabc\n\n   \t"));
+    assert::are_equal(std::string(" abc"), util::strip_back(" abc "));
+    assert::are_equal(std::string("   abc"), util::strip_back("   abc    "));
+    assert::are_equal(std::string(" \t\nabc"), util::strip_back(" \t\nabc\n\n   \t"));
 
     return 0;
 }
