85113b31803107ab07228d4f74586e5cf8c2a76e
Lukas Tietze <lukas.tietze@s2014.tu-chemnitz.de>
2018-12-05 20:47:05 +0100

    Upsi :S

---
+++ common/src/exception/Utils.cpp
@@ -0,0 +1,34 @@
+#include <sstream>
+
+#include "Exception.hpp"
+
+std::string util::FormatStacktrace(const boost::stacktrace::stacktrace &trace, uint indent, uint maxLen, bool showAdress)
+{
+    std::stringstream buf;
+    std::string indentBuffer = std::string(indent, ' ');
+
+    uint pos = 0;
+
+    for (const auto &frame : trace)
+    {
+        if (pos < 0 || pos >= maxLen)
+        {
+            break;
+        }
+
+        pos++;
+        buf << indentBuffer << pos << '#';
+        if (showAdress)
+            buf << '[' << frame.address() << ']';
+
+        buf << ' ' << frame.name();
+
+        if (frame.source_line() != 0)
+            buf << " in "
+                << frame.source_file() << ":" << frame.source_line();
+
+        buf << std::endl;
+    }
+
+    return buf.str();
+}
