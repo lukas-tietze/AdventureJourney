1ac7ca8009ec8c5000f383bc33a201b67d8ca375
Lukas Tietze <lukas.tietze@s2014.tu-chemnitz.de>
2019-01-16 15:33:33 +0100

    More Container templates

---
+++ common/include/terminal/controls/containers/LinearContainer.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "terminal/controls/containers/ContainerBase.hpp"
+
+namespace terminal
+{
+class LinearContainer : public ContainerBase
+{
+  public:
+    LinearContainer();
+};
+} // namespace terminal
