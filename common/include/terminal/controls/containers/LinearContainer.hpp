ff7c014f5df13b4938645148f86b8c8a08dd6bbf
lukas <lukas.tietzermp@gmail.com>
2019-01-16 17:04:31 +0100

    Merge2

--- common/include/terminal/controls/containers/LinearContainer.hpp
+++ common/include/terminal/controls/containers/LinearContainer.hpp
@@ -1,10 +1,6 @@
 #pragma once
 
-<<<<<<< HEAD
-#include "terminal/controls/ControlBase.hpp"
-=======
 #include "terminal/controls/containers/ContainerBase.hpp"
->>>>>>> 2eb1c4acdb40749a0ddd5eeb429b7b2e1c376271
 
 namespace terminal
 {
@@ -12,7 +8,6 @@ class LinearContainer : public ContainerBase
 {
   public:
     LinearContainer();
-<<<<<<< HEAD
     virtual ~LinearContainer();
 
     enum class Orientation
@@ -25,7 +20,5 @@ class LinearContainer : public ContainerBase
 
   private:
     Orientation orientation;
-=======
->>>>>>> 2eb1c4acdb40749a0ddd5eeb429b7b2e1c376271
 };
 } // namespace terminal
