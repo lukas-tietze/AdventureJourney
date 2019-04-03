9b6718ed654d210b1af01f88a241e7a2d6678dea
lukas <lukas.tietzermp@gmail.com>
2019-04-03 15:03:18 +0200

    Iterator Utils

--- common/include/data/IteratorUtils.hpp
+++ common/include/data/IteratorUtils.hpp
@@ -7,15 +7,31 @@ namespace util
 template <class T>
 class IteratorPrintWrapper
 {
+    friend std::ostream &operator<<(std::ostream &s, const IteratorPrintWrapper<T> &wrapper);
+
+  private:
+    T begin;
+    T end;
+    std::string seperator;
+
+  public:
+    IteratorPrintWrapper(const T &begin, const T &end, const std::string &) : begin(begin),
+                                                                              end(end),
+                                                                              seperator(seperator)
+    {
+    }
 };
 
 template <class T>
 std::ostream &operator<<(std::ostream &s, const IteratorPrintWrapper<T> &wrapper)
 {
+    for (auto pos = wrapper.begin; pos != end; ++pos)
+        s << *pos << wrapper.seperator;
 }
 
 template <class T>
-IteratorPrintWrapper<T> WrapIterable(T begin, T end)
+IteratorPrintWrapper<T> WrapIterable(T begin, T end, std::string seperator = "")
 {
+    return IteratorPrintWrapper<T>(begin, end, seperator);
 }
 } // namespace util
