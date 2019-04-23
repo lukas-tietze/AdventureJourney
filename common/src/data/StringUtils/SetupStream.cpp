ccf963aada16a5737d9ba0ef6014d0ad31e2a8b3
lukas <lukas.tietzermp@gmail.com>
2019-04-23 21:25:54 +0200

    Setupstream Flesh out

--- common/src/data/StringUtils/SetupStream.cpp
+++ common/src/data/StringUtils/SetupStream.cpp
@@ -1,8 +1,15 @@
 #include "data/String.hpp"
 
+namespace
+{
+int ReadHex(std::iostream &stream, const std::string &format)
+{
+}
+} // namespace
+
 /*
  * Format:
- *      %{[index:]options} => %{[(0-9)+:(optionslist...)]}
+ *      %{options}
  * Optionen:
  *      hex     format integer as hexadecimal number
  *      bin     format integer as binary number
@@ -14,5 +21,28 @@ void util::SetupStreamByPrintfFormat(std::iostream &stream, const std::string &f
 {
     for (int i = 0; i < format.length(); i++)
     {
+        switch (format[i])
+        {
+        case 'h':
+            break;
+        case 'o':
+            break;
+        case 'b':
+            break;
+        case '0':
+            break;
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            break;
+        default:
+            break;
+        }
     }
 }
