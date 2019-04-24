d388236c98d6e6867b9353f051f777b8703b33b0
lukas <lukas.tietzermp@gmail.com>
2019-04-24 15:50:42 +0200

    Files für VS

--- common/include/config.hpp
+++ common/include/config.hpp
@@ -1,3 +1,1 @@
-/* #undef DEBUG */
-
-#pragma once
+#define DEBUG
--- playground/main.cpp
+++ playground/main.cpp
@@ -1,33 +1,9 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <pthread.h>
 #include <cstdio>
-#include <ncurses.h>
-
-#include "data/String.hpp"
-#include "data/Io.hpp"
-
-namespace
-{
-util::Channel out;
-
-template <class T>
-void Foo(T &)
-{
-    out.WriteLine("I am not a pointer");
-}
-
-template <class T>
-void Foo(T *)
-{
-    out.WriteLine("I am a pointer");
-}
-} // namespace
 
 int main()
 {
-    int i = 0;
-
-    Foo(&i);
+	std::printf("Hello World, lol");
 }
