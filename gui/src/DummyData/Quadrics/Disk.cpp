da7ce079986583a170515d4a1e919444a76dd84d
lukas <lukas.tietzermp@gmail.com>
2019-05-21 20:02:18 +0200

    stb -> eigener Ordner1

--- gui/src/DummyData/Quadrics/Disk.cpp
+++ gui/src/DummyData/Quadrics/Disk.cpp
@@ -10,34 +10,38 @@ bool gui::quadrics::Disk(uint32_t slices, uint32_t loops, glutil::Mesh &out, con
 {
     QuadricContext q(config);
 
-    // create (loops-1) rings
-    for (uint32_t iL = 0; iL < loops; iL++)
+    q.Reserve(loops * slices + 1, slices * 3 + loops * (slices - 1));
+
+    size_t loop, slice, nextSlice;
+    float loopStep = 1.f / loops;
+    float sliceStep = static_cast<float>(1.0 / slices * 2 * M_PI);
+
+    q.SetNormal(0, 1, 0);
+    q.SetPosition(0, 0, 0);
+    q.SetTexCoords(0.5f, 0.5f);
+    q.Push();
+
+    for (slice = 0; slice < slices; slice++)
     {
-        float ro = (1.0f - float(iL) / float(loops));     // outer radius
-        float ri = (1.0f - float(iL + 1) / float(loops)); // inner radius
-        // with (slices-1) elements
-        for (uint32_t iSl = 0; iSl < slices; iSl++)
+        nextSlice = (slice + 1) % slices;
+
+        for (loop = 0; loop < loops; loop++)
         {
-            float sliceRatioL = float(iSl) / float(slices);
-            float sliceRatioR = float(iSl + 1) / float(slices);
-            float cl = cosf(sliceRatioL * (float)M_PI * 2.0f), sl = sinf(sliceRatioL * (float)M_PI * 2.0f),
-                  cr = cosf(sliceRatioR * (float)M_PI * 2.0f), sr = sinf(sliceRatioR * (float)M_PI * 2.0f);
-            if (iL == loops - 1)
-            {
-                // TODO!
-                // m_addTriangle(cl * ro, sl * ro, 0, 0, 0, 1,
-                //               cr * ro, sr * ro, 0, 0, 0, 1,
-                //               0, 0, 0, 0, 0, 1);
-            }
-            else
-            {
-                // TODO!
-                // m_addQuad(cl * ro, sl * ro, 0, 0, 0, 1,
-                //           cr * ro, sr * ro, 0, 0, 0, 1,
-                //           cr * ri, sr * ri, 0, 0, 0, 1,
-                //           cl * ri, sl * ri, 0, 0, 0, 1);
-            }
+            float a = std::sin(slice * sliceStep) * loop * loopStep;
+            float b = std::cos(slice * sliceStep) * loop * loopStep;
+
+            q.SetPosition(a, 0, b);
+            q.SetTexCoords(a, b);
+            q.Push();
         }
+
+        for (loop = 0; loop < loops - 1; loop++)
+            q.PushQuad(slice * loops * loop + 1,
+                       nextSlice * loops + loop + 1,
+                       nextSlice * loops + loop + 2,
+                       slice * loops + loop + 2);
+
+        q.PushTriangle(0, slice * loops + 1, nextSlice * loops + 1);
     }
 
     return q.CreateMesh(out);
