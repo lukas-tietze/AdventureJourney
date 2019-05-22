2f91a60c090756874dc5e53757a342bf07c4a198
Lukas Tietze <lukas.tietze@s2014.tu-chemnitz.de>
2019-05-22 12:35:14 +0200

    UV_sphere gefixt

--- gui/src/DummyData/Quadrics/UvSphere.cpp
+++ gui/src/DummyData/Quadrics/UvSphere.cpp
@@ -15,32 +15,32 @@ bool gui::quadrics::UvSphere(uint32_t slices, uint32_t stacks, glutil::Mesh &out
 
     float phi, rho;
     size_t slice, stack;
+    float sliceStep = 1.f / static_cast<float>(slices);
+    float stackStep = 1.f / static_cast<float>(stacks);
 
     ///Top
-    q.SetTexCoords(0.5f, 0.0f);
+    q.SetTexCoords(0.0f, 0.0f);
     q.SetPositionAndNormal(0.f, 1.f, 0.f);
     q.Push();
     ///Bottom
-    q.SetTexCoords(0.5f, 1.0f);
+    q.SetTexCoords(0.0f, 1.0f);
     q.SetPositionAndNormal(0.f, -1.f, 0.f);
     q.Push();
 
     for (slice = 0; slice < slices + 1; ++slice)
     {
-        phi = 2.0f * M_PI * static_cast<float>(slice) / static_cast<float>(slices);
+        phi = 2.0f * M_PI * slice * sliceStep;
 
         for (stack = 0; stack < stacks - 1; ++stack)
         {
-            rho = -M_PI / 2.0 + M_PI * (static_cast<float>(stack + 1) / static_cast<float>(stacks));
+            rho = -M_PI / 2.0 + M_PI * (stack + 1) * stackStep;
 
             q.SetPositionAndNormal(cos(rho) * cos(phi), sin(rho), cos(rho) * sin(phi));
-            q.SetSphericalTexCoords();
+            q.SetTexCoords(slice * sliceStep, stack * stackStep);
             q.Push();
         }
 
-        ///Texturkoordinaten werden 0 bei y = 0!
-
-        if (slice < slices - 1)
+        if (slice < slices)
         {
             for (stack = 0; stack < stacks - 2; ++stack)
             {
