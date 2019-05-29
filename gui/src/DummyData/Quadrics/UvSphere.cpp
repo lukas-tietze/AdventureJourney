59680c930cb49e69851a5574b1e586a382fc3a68
lukas <lukas.tietzermp@gmail.com>
2019-05-29 12:59:01 +0200

    besseres, aber immernoch nicht perfektes Texturieren von UVs

--- gui/src/DummyData/Quadrics/UvSphere.cpp
+++ gui/src/DummyData/Quadrics/UvSphere.cpp
@@ -18,41 +18,52 @@ bool gui::quadrics::UvSphere(uint32_t slices, uint32_t stacks, glutil::Mesh &out
     float sliceStep = 1.f / static_cast<float>(slices);
     float stackStep = 1.f / static_cast<float>(stacks);
 
-    ///Top
-    q.SetTexCoords(0.0f, 0.0f);
-    q.SetPositionAndNormal(0.f, 1.f, 0.f);
-    q.Push();
-    ///Bottom
-    q.SetTexCoords(0.0f, 1.0f);
-    q.SetPositionAndNormal(0.f, -1.f, 0.f);
-    q.Push();
-
     for (slice = 0; slice < slices + 1; ++slice)
     {
-        phi = 2.0f * M_PI * slice * sliceStep;
-
-        for (stack = 0; stack < stacks - 1; ++stack)
+        ///vertices
         {
-            rho = -M_PI / 2.0 + M_PI * (stack + 1) * stackStep;
+            ///Bottom
+            q.SetPositionAndNormal(0.f, -1.f, 0.f);
+            q.SetTexCoords(slice * sliceStep, 1.0f);
+            q.Push();
 
-            q.SetPositionAndNormal(cos(rho) * cos(phi), sin(rho), cos(rho) * sin(phi));
-            q.SetTexCoords(slice * sliceStep, stack * stackStep);
+            phi = 2.0f * M_PI * slice * sliceStep;
+
+            for (stack = 0; stack < stacks - 1; ++stack)
+            {
+                rho = -M_PI / 2.0 + M_PI * (stack + 1) * stackStep;
+
+                q.SetPositionAndNormal(cos(rho) * cos(phi), sin(rho), cos(rho) * sin(phi));
+                q.SetTexCoords(slice * sliceStep, stack * stackStep);
+                q.Push();
+            }
+
+            ///Top
+            q.SetPositionAndNormal(0.f, 1.f, 0.f);
+            q.SetTexCoords(slice * sliceStep, 0.0f);
             q.Push();
         }
-
-        if (slice < slices)
+        ///indices
         {
-            for (stack = 0; stack < stacks - 2; ++stack)
+            if (slice < slices)
             {
-                q.PushQuad(slice * (stacks - 1) + stack + 2,
-                           (slice + 1) * (stacks - 1) + stack + 2,
-                           (slice + 1) * (stacks - 1) + (stack + 1) + 2,
-                           slice * (stacks - 1) + (stack + 1) + 2);
+                for (stack = 1; stack < stacks - 1; ++stack)
+                {
+                    q.PushQuad(slice * (stacks + 1) + stack,
+                               (slice + 1) * (stacks + 1) + stack,
+                               (slice + 1) * (stacks + 1) + stack + 1,
+                               slice * (stacks + 1) + stack + 1);
+                }
+                
+                q.PushTriangle(slice * (stacks + 1),
+                               slice * (stacks + 1) + 1,
+                               (slice + 1) * (stacks + 1) + 1);
+
+                q.PushTriangle(slice * (stacks + 1) + stacks,
+                               slice * (stacks + 1) + stacks - 1,
+                               (slice + 1) * (stacks + 1) + stacks - 1);
             }
         }
-
-        q.PushTriangle(1, slice * (stacks - 1) + 2, (slice + 1) * (stacks - 1) + 2);
-        q.PushTriangle(0, slice * (stacks - 1) + stacks, (slice + 1) * (stacks - 1) + stacks);
     }
 
     return q.CreateMesh(out);
